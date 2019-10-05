0f842a57153142532914f61b2647d3b3f540eee4
Bill70058 <[Lin_k_Bill@163.com]>
2019-10-05 22:36:03 +0800

    fib -- update

--- 509.fib/509.fib.js
+++ 509.fib/509.fib.js
@@ -3,14 +3,21 @@
  * @return {number}
  */
 var fib = function(N) {
-    if(N === 0 ){
-        return N;
+    // if(N === 0 ){
+    //     return N;
+    // }
+    // let a1 = 0,a2 = 1;
+    // for(let i = 1; i < N; i++){
+    //     [a1,a2] = [a2,a1+a2]; // => a2 = a1+a2; a1 = a2-a1;
+    // }
+    // return a2;
+
+    if(N === 1|| N === 2){
+        return 1;
     }
-    let a1 = 0,a2 = 1;
-    for(let i = 1; i < N; i++){
-        [a1,a2] = [a2,a1+a2]; // => a2 = a1+a2; a1 = a2-a1;
-    }
-    return a2;
+    return fib(N-1)+fib(N-2);
 };
 
 module.exports = fib;
+
+console.log(fib(3));
