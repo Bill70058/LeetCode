bee61242be96ef32fc06d065f62b2e25433b39a1
Bill70058 <[Lin_k_Bill@163.com]>
2019-10-08 14:15:10 +0800

    update

--- 35.search-insert/35.search-insert.js
+++ 35.search-insert/35.search-insert.js
@@ -7,15 +7,17 @@ var searchInsert = function(nums, target) {
     let index = nums.indexOf(target);
     if(index > -1){
         return index;
-    }else if(nums[nums.length-1] < target) {
+    }
+    if(nums[nums.length-1] < target) {
            return nums.length;
-    }else {
-        for(let i = 0,len = nums.length; i < len; i++){
-            if(nums[i] > target){
-                return i;
-            }
+    }
+    
+    for(let i = 0,len = nums.length; i < len; i++){
+        if(nums[i] > target){
+            return i;
         }
     }
+    
 };
 
 console.log(searchInsert([1,3,5,6],5))
