d4373d3207b9fd27ca720126553414ccb3bf6468
Bill70058 <[Lin_k_Bill@163.com]>
2019-10-08 18:11:46 +0800

    relative sort array

--- 1122.relative-sort-array/1122.relative-sort-array.js
+++ 1122.relative-sort-array/1122.relative-sort-array.js
@@ -3,7 +3,7 @@
  * @param {number[]} arr2
  * @return {number[]}
  */
-var relativeSortArray = function(arr1, arr2) {
+const relativeSortArray = function(arr1, arr2) {
     let temp = [];
     let array = [];
     for(let n of arr1){
@@ -16,7 +16,7 @@ var relativeSortArray = function(arr1, arr2) {
     });
     for(let i = 0; i < arr2.length; i++){
         for(let j = 0; j < arr1.length; j++){
-            if(arr2[i] == arr1[j]){
+            if(arr2[i] === arr1[j]){
                 array.push(arr1[j]);
             }
         }
@@ -27,7 +27,5 @@ var relativeSortArray = function(arr1, arr2) {
   return array;  
 };
 
-// console.log(relativeSortArray([28,6,22,8,44,17],[22,28,8,6]))
-// relativeSortArray([2,3,1,3,2,4,6,7,9,2,19],[2,1,4,3,9,6])
 
 module.exports = relativeSortArray;
