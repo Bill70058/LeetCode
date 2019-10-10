ff77d1c01b830577d955d8aeccd35787eaac9245
Bill70058 <[Lin_k_Bill@163.com]>
2019-10-10 20:25:45 +0800

    can place flowers

--- 605.can-place-flowers/605.can-place-flowers.js
+++ 605.can-place-flowers/605.can-place-flowers.js
@@ -5,9 +5,10 @@
  */
 var canPlaceFlowers = function(flowerbed, n) {
     for (let i = 0; i < flowerbed.length; i++) {
-         if (flowerbed[i] == 0 
-         && (i - 1 == -1 || flowerbed[i - 1] == 0) 
-         && (i + 1 == flowerbed.length || flowerbed[i + 1] == 0)) {
+        // 判断 i-1 与 i+1 是对数组边界的两种特殊情况进行判断
+         if (flowerbed[i] === 0 
+         && (i - 1 === -1 || flowerbed[i - 1] === 0) 
+         && (i + 1 === flowerbed.length || flowerbed[i + 1] === 0)) {
              n--;
              flowerbed[i] = 1;
          }
