9c480de48d6c4b6b2b75f6f4e9cfc12deae85d30
Bill70058 <[Lin_k_Bill@163.com]>
2019-10-11 09:44:09 +0800

    update

--- 628.maximum-product/628.maximum-product.js
+++ 628.maximum-product/628.maximum-product.js
@@ -2,8 +2,8 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maximumProduct = function(nums) {
-    nums.sort((a,b)=>{return a-b});
+const maximumProduct = function(nums) {
+    nums.sort((a,b)=>a-b);
     let len = nums.length;
     let moduleOne = nums[len-1]*nums[len-2]*nums[len-3],moduleTwo = nums[0]*nums[1]*nums[len-1];
     return Math.max(moduleOne,moduleTwo);
