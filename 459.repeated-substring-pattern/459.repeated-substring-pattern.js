24f41edb942707e022e078eba3675a60b398fa37
Bill70058 <[Lin_k_Bill@163.com]>
2019-10-28 19:42:33 +0800

    add a new answer

--- 459.repeated-substring-pattern/459.repeated-substring-pattern.js
+++ 459.repeated-substring-pattern/459.repeated-substring-pattern.js
@@ -5,6 +5,7 @@
 const repeatedSubstringPattern = function(s) {
     let sCopy = (s+s).slice(1,-1);
     return sCopy.includes(s);
+    //    return (s+s).substring(1,s.length*2 -1).indexOf(s)!=-1;
 };
 
 // console.log(repeatedSubstringPattern('abab'));
