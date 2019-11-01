907b7e4744f01fae6ed6bf32616f284a39c80499
Bill70058 <[Lin_k_Bill@163.com]>
2019-11-01 15:12:16 +0800

    longest word

--- 720.longest-word/720.longest-word.js
+++ 720.longest-word/720.longest-word.js
@@ -7,6 +7,7 @@ var longestWord = function (words) {
     let res = '';
     words = words.sort();
     for (let word of words) {
+        //如果单词只有一个字母，那一定是共有的
         if (word.length === 1 || map.has(word.substring(0, word.length - 1))) {
             res = word.length > res.length ? word : res;
             map.set(word);
