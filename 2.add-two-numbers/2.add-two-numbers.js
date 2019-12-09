3daade05f173badad527e438835b209252e22653
Bill70058 <Lin_k_Bill@163.com>
2019-12-09 22:35:17 +0800

    add two numbers

---
+++ 2.add-two-numbers/2.add-two-numbers.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+    let result;
+    let cache = [];
+    let flag = 0;
+
+    while (l1 || l2) {
+        let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + flag;
+        if (sum > 9) {
+            sum -= 10;
+            flag = 1;
+        } else {
+            flag = 0;
+        }
+
+        let list = new ListNode(sum);
+        if (cache[0]) {
+            cache[0].next = list;
+            cache[0] = list;
+        } else {
+            cache[0] = result = list;
+        }
+
+        l1 = l1 && l1.next;
+        l2 = l2 && l2.next;
+    }
+
+    if (flag === 1) {
+        cache[0].next = new ListNode(1);
+    }
+    return result;
+};
