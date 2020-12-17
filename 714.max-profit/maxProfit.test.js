d01aa1bad07a41209ac99583ab912f28e7045dbb
Bill70058 <Lin_k_Bill@163.com>
2020-12-17 17:39:48 +0800

    max profit

---
+++ 714.max-profit/maxProfit.test.js
@@ -0,0 +1,7 @@
+import maxProfit from './maxProfit'
+
+describe('monotone increasingDigits', () => {
+  test('input [1,3,2,8,4,9],2 return 8', () => {
+    expect(maxProfit([1, 3, 2, 8, 4, 9], 2)).toEqual(8)
+  })
+})
